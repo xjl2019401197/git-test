25376a50b580b329724053dc5ed67a242dfbc6b3
xjl2019401197 <2550908862@qq.com>
2022-03-21 20:42:33 +0800

    master第二次提交

--- src/main/java/com/xu/git/GitTest.java
+++ src/main/java/com/xu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello Git2");
         System.out.println("hello Git2");
         System.out.println("hello Git2");
+        System.out.println("hello master");
     }
 }
